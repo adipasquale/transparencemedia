9b9d2747527f77c29607f27a0401c8dfdfc6c27d
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2022-01-19 10:51:38 +0100

    simplified data

--- processor/lib/entites.js
+++ processor/lib/entites.js
@@ -1,15 +1,14 @@
 import fs from 'fs'
 import slugify from "slugify"
+import matter from 'gray-matter'
+import yaml from "js-yaml"
 
 import rootPath from "./rootPath.js"
-import matter from 'gray-matter'
 
-let actionnaires = JSON.parse(fs.readFileSync(`${rootPath}/data/actionnaires.json`))
+let actionnaires = yaml.load(fs.readFileSync(`${rootPath}/data/actionnaires.yaml`))
 let journaux = fs.readdirSync(`${rootPath}/www/journaux/`)
   .map(fn => matter(fs.readFileSync(`${rootPath}/www/journaux/${fn}`)))
   .map(gm => gm.data)
-// console.log("actionnaires", actionnaires)
-// console.log("journaux", journaux)
 let entites = actionnaires.concat(journaux)
 
 const unslugify = slug => slug
--- www/.eleventy.js
+++ www/.eleventy.js
@@ -5,7 +5,4 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
   eleventyConfig.addNunjucksFilter("stringify", o => JSON.stringify(o));
-  eleventyConfig.addNunjucksFilter("sortEntites", (entites) => {
-    return entites.sort((a, b) => a.id < b.id ? -1 : 1)
-  });
 };
